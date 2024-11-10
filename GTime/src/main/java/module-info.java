5f244fb5f59cb31d080edc126dd63df16956b100
Trabajo <Trabajo@DESKTOP-E7E14D7>
2024-11-10 21:19:54 +0100

    VERSION ESTABLE 0.5

--- GTime/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.GTime.GTime {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires java.sql;
-	requires java.desktop;
-
-    opens GTime to javafx.fxml;
-    exports GTime;
-}
--- GTime/src/main/java/utilidades/SCRUDusuarios.java
+++ GTime/src/main/java/utilidades/SCRUDusuarios.java
@@ -6,6 +6,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.validator.routines.RegexValidator;
+
+
 
 
 public class SCRUDusuarios {	
@@ -78,6 +82,41 @@ public void registerSCRUD(String nombre,String apellidos,String mail,String nomb
 	}
 }
 
+public static String validacionFormulario(String nombreUsuario, String apellidos, String mail, String nombreReal, String contrasenia, String confContrasenia)  {
+	
+	// Validacion de correo electrónico (funcion de commons validator)
+	
+			EmailValidator emailValidator = EmailValidator.getInstance();
+			
+			// Crear validadores con expresiones regulares para cada requisito
+	        RegexValidator mayusculaValidator = new RegexValidator(".[A-Z].");  // Al menos una mayúscula
+	        RegexValidator minusculaValidator = new RegexValidator(".[a-z].");  // Al menos una minúscula
+	        RegexValidator numeroValidator = new RegexValidator(".[0-9].");      // Al menos un dígito
+	        RegexValidator especialValidator = new RegexValidator(".[^A-Za-z0-9]."); // Al menos un carácter especial
+	        RegexValidator noNumerosValidator = new RegexValidator("^[^0-9]*$");
+	        
+	        if (nombreUsuario.isEmpty()) {
+	            return "Por favor, rellene el campo 'nombre'.";
+	        } else if (apellidos.isEmpty()) {
+	            return "Por favor, rellene el campo 'apellidos'.";
+	        } else if (!emailValidator.isValid(mail)) {
+	            return "Correo electrónico no válido.";
+	        } else if (!noNumerosValidator.isValid(nombreReal) || !especialValidator.isValid(nombreReal)) {
+				return "El nombre real no puede tener numeros o caracteres especiales";
+			} else if (!contrasenia.equals(confContrasenia)) {
+	            return "Las contraseñas no coinciden.";
+	        } else if (!mayusculaValidator.isValid(confContrasenia)) { // Verificar si la contraseña tiene una mayúscula
+	            return "La contraseña debe contener al menos una letra mayúscula, además de una minúscula, un número y un carácter especial.";
+	        } else if (!minusculaValidator.isValid(confContrasenia)) { // Verificar si tiene al menos una minúscula
+	            return "La contraseña debe contener al menos una letra minúscula, además de una mayúscula, un número y un carácter especial.";
+	        } else if (!numeroValidator.isValid(confContrasenia)) {
+	            return "La contraseña debe contener al menos un número, además de una mayúscula, una minúscula y un carácter especial.";
+	        } else if (!especialValidator.isValid(confContrasenia)) {
+	            return "La contraseña debe contener al menos un carácter especial, además de una mayúscula, una minúscula y un número.";
+	        }
+
+	        return "Registro exitoso.";
+	    }
 
 
 

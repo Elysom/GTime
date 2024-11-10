f3933051a186afa868b2b5a722b63acbc51ce248
Trabajo <Trabajo@DESKTOP-E7E14D7>
2024-11-10 19:27:59 +0100

    Version con Cambios VALIDATOR

--- GTime/src/main/java/GTime/controladorRegister.java
+++ GTime/src/main/java/GTime/controladorRegister.java
@@ -2,9 +2,10 @@ package GTime;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import utilidades.SCRUDusuarios;
-
 import org.apache.commons.validator.routines.EmailValidator;
 import org.apache.commons.validator.routines.RegexValidator;
 
@@ -24,7 +25,8 @@ public class controladorRegister {
 	TextField txtContrasenia;
 	@FXML
 	TextField txtConfContrasenia;
-	
+	@FXML
+	public static Text txtValidacion;
 	
 	
 	private void register() {
@@ -35,11 +37,48 @@ public class controladorRegister {
 		String contrasenia=txtContrasenia.getText();
 		String confContrasenia=txtContrasenia.getText();
 		
-		String resultadoValidacion = SCRUDusuarios.validacionFormulario(nombre, apellidos, mail, nombreReal, contrasenia, confContrasenia);
+		validacionRegister(nombre, apellidos, mail, nombreReal, contrasenia, confContrasenia);
 		
 		
 	}
+	//SEPARAR LOS METODOS DE LA VALIDACION POR CAMPOS(UN METODO PARA EL NOMBRE, OTRO PARA EL APELLIDO ETC)
 	
+	
+	public static void validacionRegister(String nombreUsuario, String apellidos, String mail, String nombreReal, String contrasenia, String confContrasenia)  {
+		
+		// Validacion de correo electrónico (funcion de commons validator)
+		
+				EmailValidator emailValidator = EmailValidator.getInstance();
+				
+				// Crear validadores con expresiones regulares para cada requisito
+		        RegexValidator mayusculaValidator = new RegexValidator(".*[A-Z].*");  // Al menos una mayúscula
+		        RegexValidator minusculaValidator = new RegexValidator(".*[a-z].*");  // Al menos una minúscula
+		        RegexValidator numeroValidator = new RegexValidator(".*[0-9].*");      // Al menos un dígito
+		        RegexValidator especialValidator = new RegexValidator(".*[^A-Za-z0-9].*"); // Al menos un carácter especial
+		        RegexValidator noNumerosValidator = new RegexValidator("^[^0-9]*$");
+		        
+		        if (nombreUsuario.isEmpty()) {
+		        	txtValidacion.setText("Por favor, rellene el campo 'nombre'.");
+		        } else if (apellidos.isEmpty()) {
+		        	txtValidacion.setText("Por favor, rellene el campo 'apellidos'.");
+		        } else if (!emailValidator.isValid(mail)) {
+		        	txtValidacion.setText("Correo electrónico no válido.");
+		        } else if (!noNumerosValidator.isValid(nombreReal) || !especialValidator.isValid(nombreReal)) {
+		        	txtValidacion.setText("El nombre real no puede tener numeros o caracteres especiales"); 
+				} else if (!contrasenia.equals(confContrasenia)) {
+					txtValidacion.setText("Las contraseñas no coinciden."); 
+		        } else if (!mayusculaValidator.isValid(confContrasenia)) { // Verificar si la contraseña tiene una mayúscula
+		        	txtValidacion.setText("La contraseña debe contener al menos una letra mayúscula, además de una minúscula, un número y un carácter especial."); 
+		        } else if (!minusculaValidator.isValid(confContrasenia)) { // Verificar si tiene al menos una minúscula
+		        	txtValidacion.setText("La contraseña debe contener al menos una letra mayúscula, además de una minúscula, un número y un carácter especial.");
+		        } else if (!numeroValidator.isValid(confContrasenia)) {
+		        	txtValidacion.setText("La contraseña debe contener al menos un número, además de una mayúscula, una minúscula y un carácter especial.");
+		        } else if (!especialValidator.isValid(confContrasenia)) {
+		        	txtValidacion.setText("La contraseña debe contener al menos un carácter especial, además de una mayúscula, una minúscula y un número.");
+		        }
+
+		     
+		    }
 
 
 }
--- GTime/src/main/java/utilidades/SCRUDusuarios.java
+++ GTime/src/main/java/utilidades/SCRUDusuarios.java
@@ -9,6 +9,8 @@ import java.time.LocalDate;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.apache.commons.validator.routines.RegexValidator;
 
+import GTime.controladorRegister;
+
 
 
 public class SCRUDusuarios {	
@@ -58,46 +60,27 @@ public void loginSCRUD(String usuario,String contrasenia) {
 	
 	
 }
-public void registerSCRUD(){
+public void registerSCRUD(String nombre,String apellidos,String mail,String nombreReal,String contrasenia){
 	
+	Connection cn= utilidades.DatabaseConnector.dameConexion();
+	//Logica para la insercion del usuario  en la base de datos
+	String sql = "INSERT INTO usuarioslista (nombre, apellidos, mail, nombreReal, contrasenia) VALUES (?, ?, ?, ?, ?)";
+	
+	try (PreparedStatement StatementRegister = cn.prepareStatement(sql)){
+		
+		StatementRegister.setString(1, nombre);
+		StatementRegister.setString(2, apellidos);
+		StatementRegister.setString(3, mail);
+		StatementRegister.setString(4, nombreReal);
+		StatementRegister.setString(5, contrasenia);
+		StatementRegister.executeQuery();
+		System.out.println("Usuario  registrado");
+		StatementRegister.close();
+	} catch (Exception e) {
+		// TODO: handle exception
+        System.out.println("Error al registrarse: " + e.getMessage());
+
+	}
 }
-public static String validacionFormulario(String nombreUsuario, String apellidos, String mail, String nombreReal, String contrasenia, String confContrasenia)  {
-	
-	// Validacion de correo electrónico (funcion de commons validator)
-	
-			EmailValidator emailValidator = EmailValidator.getInstance();
-			
-			// Crear validadores con expresiones regulares para cada requisito
-	        RegexValidator mayusculaValidator = new RegexValidator(".*[A-Z].*");  // Al menos una mayúscula
-	        RegexValidator minusculaValidator = new RegexValidator(".*[a-z].*");  // Al menos una minúscula
-	        RegexValidator numeroValidator = new RegexValidator(".*[0-9].*");      // Al menos un dígito
-	        RegexValidator especialValidator = new RegexValidator(".*[^A-Za-z0-9].*"); // Al menos un carácter especial
-	        RegexValidator noNumerosValidator = new RegexValidator("^[^0-9]*$");
-	        
-	        if (nombreUsuario.isEmpty()) {
-	            return "Por favor, rellene el campo 'nombre'.";
-	        } else if (apellidos.isEmpty()) {
-	            return "Por favor, rellene el campo 'apellidos'.";
-	        } else if (!emailValidator.isValid(mail)) {
-	            return "Correo electrónico no válido.";
-	        } else if (!noNumerosValidator.isValid(nombreReal) || !especialValidator.isValid(nombreReal)) {
-				return "El nombre real no puede tener numeros o caracteres especiales";
-			} else if (!contrasenia.equals(confContrasenia)) {
-	            return "Las contraseñas no coinciden.";
-	        } else if (!mayusculaValidator.isValid(confContrasenia)) { // Verificar si la contraseña tiene una mayúscula
-	            return "La contraseña debe contener al menos una letra mayúscula, además de una minúscula, un número y un carácter especial.";
-	        } else if (!minusculaValidator.isValid(confContrasenia)) { // Verificar si tiene al menos una minúscula
-	            return "La contraseña debe contener al menos una letra minúscula, además de una mayúscula, un número y un carácter especial.";
-	        } else if (!numeroValidator.isValid(confContrasenia)) {
-	            return "La contraseña debe contener al menos un número, además de una mayúscula, una minúscula y un carácter especial.";
-	        } else if (!especialValidator.isValid(confContrasenia)) {
-	            return "La contraseña debe contener al menos un carácter especial, además de una mayúscula, una minúscula y un número.";
-	        }
-
-	        return "Registro exitoso.";
-	    }
-
-
-
 
 }
